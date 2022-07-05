370703ffa01a5093bcd6910e1926e28dd9957ba8
Ian Davids <ian@algoriststudios.com>
2022-07-05 15:55:18 -0700

    Todos

--- web/todos.js
+++ web/todos.js
@@ -8,6 +8,28 @@ var todos_section_loaded = (jrpc) => {
 
 var todosInit = () => {
     for (var entry in todo_lists) {
-      console.log(entry);
+        let el = document.getElementById("TodosList");
+        if (el != null) {
+
+            let node = document.createElement("a");
+            node.className = "collapse-item";
+            node.innerHTML = `${entry[0].toUpperCase()}${entry.slice(1)}`;
+            let v = entry;
+
+            node.onclick = () => { console.log(v); ShowTodos(v,todo_lists[v]); return false;};
+            el.appendChild(node);
+        }
+        // <a class="collapse-item" onclick="ShowPage('todos_section');return false" href="buttons.html">Buttons</a>
+        console.log(entry);
+    }
+}
+
+var ShowTodos = (name,query) => {
+    console.log(name);
+    console.log(query);
+    ShowPage("todos_section");
+    let tit = document.getElementById('TodoListTitle');
+    if (tit != null) {
+        tit.innerHTML = name[0].toUpperCase() + name.slice(1);
     }
 }
