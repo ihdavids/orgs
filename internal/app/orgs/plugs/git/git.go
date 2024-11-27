43bc856bcef6f7aa8c162f97d9cc712b3391bcf0
Ian Davids <idavids@roblox.com>
2024-11-27 10:54:23 -0800

    Fixing autosync I hope

--- internal/app/orgs/plugs/git/git.go
+++ internal/app/orgs/plugs/git/git.go
@@ -1,23 +1,33 @@
 package git
 
+// SETUP
+// Will use the first directory in your OrgDirs if a directory is not provided
+// Can only sync one dir.
+// plugins:
+//   - name: git
+//     freq: 300
+//     gitpath: "C:/Program Files/Git/bin/git.exe"
+
 import (
 	"bytes"
 	"fmt"
 	"io"
 	"log"
 	"os/exec"
+	"path/filepath"
 
 	"github.com/ihdavids/orgs/internal/app/orgs/plugs"
 )
 
 type Git struct {
-	Name    string
-	GitPath string
-	ok      bool
+	Name        string
+	GitPath     string
+	OrgsSyncDir string
+	ok          bool
 }
 
 func (self *Git) haveChanges() bool {
-	cmd := exec.Command("git diff --exit-code")
+	cmd := exec.Command("%s diff --exit-code", self.GitPath)
 	if err := cmd.Run(); err != nil {
 		if exitError, ok := err.(*exec.ExitError); ok {
 			if exitError.ExitCode() > 0 {
@@ -54,7 +64,7 @@ func countLines(r io.Reader) (int, error) {
 
 func (self *Git) localChanges() bool {
 	// git ls-files --other --exclude-standard --directory | wc -l
-	cmd := exec.Command("git ls-files --other --exclude-standard --directory")
+	cmd := exec.Command(self.GitPath, "ls-files", "--other", "--exclude-standard", "--directory")
 	if out, err := cmd.Output(); err == nil {
 		if lc, e := countLines(bytes.NewReader(out)); e != nil {
 			return lc > 0
@@ -64,33 +74,44 @@ func (self *Git) localChanges() bool {
 }
 
 func (self *Git) gitAddAll() bool {
-	cmd := exec.Command("git add --all")
-	if err := cmd.Run(); err == nil {
+	cmd := exec.Command(self.GitPath, "add", "--all")
+	cmd.Dir = self.OrgsSyncDir
+	var err error = nil
+	if err = cmd.Run(); err == nil {
 		return true
+	} else {
+		fmt.Printf("Git Add All Failed: %s\n", err)
 	}
 	return false
 }
 
 func (self *Git) gitCommit(msg string) bool {
-	cmd := exec.Command(fmt.Sprintf("git commit -m \"%s\"", msg))
-	if err := cmd.Run(); err == nil {
+	cmd := exec.Command(self.GitPath, "commit", "-m", fmt.Sprintf("\"%s\"", msg))
+	cmd.Dir = self.OrgsSyncDir
+	var err error = nil
+	if err = cmd.Run(); err == nil {
 		return true
+	} else {
+		fmt.Printf("Git commit error: %s\n", err)
 	}
 	return false
 }
 
 func (self *Git) gitPull() bool {
-	cmd := exec.Command(fmt.Sprintf("git pull --no-edit"))
+	cmd := exec.Command(self.GitPath, "pull", "--no-edit")
+	cmd.Dir = self.OrgsSyncDir
 	var err error = nil
 	if err = cmd.Run(); err == nil {
 		return true
+	} else {
+		fmt.Printf("Git pull error: %v\n", err)
 	}
-	fmt.Printf("Error: %v\n", err)
 	return false
 }
 
 func (self *Git) gitPush() bool {
-	cmd := exec.Command(fmt.Sprintf("git push"))
+	cmd := exec.Command(self.GitPath, "push")
+	cmd.Dir = self.OrgsSyncDir
 	if err := cmd.Run(); err == nil {
 		return true
 	}
@@ -102,31 +123,42 @@ func (self *Git) Unmarshal(unmarshal func(interface{}) error) error {
 }
 
 func (self *Git) Update(db plugs.ODb) {
-	fmt.Printf("Git Update...\n")
 	if self.ok {
+		fmt.Printf("Git Update...\n")
 		modified := self.haveChanges()
 		localChanges := self.localChanges()
 		if modified || localChanges {
+			fmt.Printf("Git found local changes, checking in...\n")
 			self.gitAddAll()
 			self.gitCommit("auto checkin from orgs")
 		}
 		fmt.Printf("Git pull...\n")
 		self.gitPull()
 		if modified || localChanges {
+			fmt.Printf("Git have changes pushing...\n")
 			self.gitPush()
 		}
 	} else {
-		fmt.Printf("Git not okay...\n")
+		fmt.Printf("Git update skipped, not okay... is path setup correctly?\n")
 	}
 }
 
 func (self *Git) Startup(freq int, manager *plugs.PluginManager, opts *plugs.PluginOpts) {
-	_, err := exec.LookPath(self.GitPath)
+	gitPath, err := exec.LookPath(self.GitPath)
 	if err != nil {
 		log.Printf("Failed to find git during git startup! ABORT")
 		log.Fatal(err)
 		self.ok = false
 	} else {
+		self.GitPath = filepath.FromSlash(gitPath)
+		fmt.Printf("Git module okay: %s\n", self.GitPath)
+		if self.OrgsSyncDir == "" {
+			self.OrgsSyncDir = manager.OrgDirs[0]
+			self.OrgsSyncDir = filepath.FromSlash(self.OrgsSyncDir)
+			fmt.Printf("Git sync dir set to: %s\n", self.OrgsSyncDir)
+		} else {
+			self.OrgsSyncDir = filepath.FromSlash(self.OrgsSyncDir)
+		}
 		self.ok = true
 	}
 }
