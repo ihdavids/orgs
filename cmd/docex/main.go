de80b26e4f1e5fc8a66c6bcb1a80b2045548d3d7
Ian Davids <ihdavids@gmail.com>
2025-04-05 20:18:29 -0700

    Working on new sorted grouping for docex

--- cmd/docex/main.go
+++ cmd/docex/main.go
@@ -7,40 +7,102 @@ import (
 	"bufio"
 	"path/filepath"
 	"strings"
+	"regexp"
+	"sort"
 )
 
 func IsGoFile(filename string) bool {
 	return filepath.Ext(filename) == ".go"
 }
 
-func ProcessFile(filename string, output string) {
+var groups map[string]string = map[string]string{}
+
+
+func ReMatch(regEx, txt string) (map[string]string) {
+
+	var compRegEx = regexp.MustCompile(regEx)
+	match := compRegEx.FindStringSubmatch(txt)
+
+	// Return nil if we did not match anything!
+	if match == nil || len(match) <= 0 {
+		return nil
+	}
+
+	empty := true
+	for _, m := range match {
+		if m != "" {
+			empty = false
+			break
+		}
+	}
+	if empty {
+		return nil
+	}
+	// Otherwise build a param map out of the match
+	paramsMap := make(map[string]string)
+	for i, name := range compRegEx.SubexpNames() {
+		if i > 0 && i <= len(match) {
+			paramsMap[name] = match[i]
+		}
+	}
+	return paramsMap
+}
+
+func ProcessFile(filename string) {
 	fmt.Printf("Processing %s\n", filename)
 	if r, err := os.Open(filename); err == nil {
 		defer r.Close()
 		scanner := bufio.NewScanner(r)
 		inDoc := false
+		docName := ""
 		out := ""
 		for scanner.Scan() {
 			line := scanner.Text()
 			if inDoc {
-				if strings.HasPrefix(line, "EDOC */") {
+				if m := ReMatch(`^\s*EDOC\s+[*]/`, line); m != nil {
 					inDoc = false
+					if docName == "" {
+						docName = "General"
+					}
+					groups[docName] = out
+					out = ""
+					docName = ""
 				} else {
+					// Reindent for grouping
+					if m := ReMatch(`^\s*(?P<stars>[*]+)\s+(?P<heading>.+)`, line); m != nil {
+						line = m["stars"] + "* " + m["heading"]
+					}
 					out += line + "\n"
 				}
 
 			} else {
-				if strings.HasPrefix(line, "/* SDOC") {
+				if m := ReMatch(`^\s*/[*]\s+SDOC(([:]\s+(?P<group>[a-zA-Z0-9 ]+)\s*)|(?P<nogroup>\s+)`, line); m != nil {
 					inDoc = true
+					ok := false
+					if docName,ok = m["group"]; !ok {
+						docName = "General"
+					}
 				}
 			}
 		}
-		if out != "" {
-			if f, err := os.OpenFile(output, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600); err == nil {
-				defer f.Close()
-				f.WriteString(out)
-				f.WriteString("\n")
-			}
+	}
+}
+
+func WriteDocs(output string) {
+	if f, err := os.OpenFile(output, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600); err == nil {
+		defer f.Close()
+		keys := make([]string, len(groups))
+		for k,_ := range groups {
+			keys = append(keys, k)
+		}
+		sort.Slice(keys, func(i, j int) bool { return strings.ToLower(keys[i]) < strings.ToLower(keys[j]) }) 
+		for _,k := range keys {
+			v := groups[k]
+			f.WriteString("* ")
+			f.WriteString(k)
+			f.WriteString("\n")
+			f.WriteString(v)
+			f.WriteString("\n")
 		}
 	}
 }
@@ -74,6 +136,7 @@ func main() {
 		return
 	}
 	for _, file := range files {
-		ProcessFile(file, output)
+		ProcessFile(file)
 	}
+	WriteDocs(output)
 }
